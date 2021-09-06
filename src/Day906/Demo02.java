6f82abc98648772a04ea99e652d753b652af762c
Beet-le <1693332907@qq.com>
2021-09-06 20:23:52 +0800

    Day906

--- src/Day906/Demo02.java
+++ src/Day906/Demo02.java
@@ -1,12 +1,8 @@
 package Day906;
-
 import java.util.Scanner;
-
 public class Demo02 {
-
-
     public static void main(String[] args) {
-        int allday = 0;
+        int AllDay = 0;
         int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         Scanner read = new Scanner(System.in);
         System.out.print("请输入年月日，格式如：2000/01/01:");
@@ -16,12 +12,12 @@ public class Demo02 {
         int month = Integer.parseInt(dateInt[1]);
         int day = Integer.parseInt(dateInt[2]);
         for (int i = 0; i < month - 1; i++) {
-            allday += days[i];
+            AllDay += days[i];
         }
-        allday += day;
-        if ((((year % 4 == 0 && year % 100 == 0) || year % 400 == 0) && month > 2)) {
-            allday++;
+        AllDay += day;
+          if ((((year % 4 == 0 && year % 100 == 0) || year % 400 == 0) && month > 2)) {
+            AllDay++;
         }
-        System.out.println(month + "月" + day + "是" + year + "年的第" + allday + "天");
+        System.out.println(month + "月" + day + "是" + year + "年的第" + AllDay + "天");
     }
 }
