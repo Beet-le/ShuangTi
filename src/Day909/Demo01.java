f5af8d7bcb974f1be41eadc9bf26a1997647c870
Beet-le <1693332907@qq.com>
2021-09-09 19:51:00 +0800

    day909

--- src/Day909/Demo01.java
+++ src/Day909/Demo01.java
@@ -3,9 +3,12 @@ package Day909;
 public class Demo01 {
     public static void main(String[] args) {
         int usename=18,paseword=184;
-      if(usename==18&&paseword==183)
+      if(usename==18&&paseword==184)
         {
-            System.out.println();
+            System.out.println("Welcom");
+        }else
+        {
+          System.out.println("GUN");
         }
     }
 }
---
+++ src/Day909/Demo02.java
@@ -0,0 +1,25 @@
+package Day909;
+import java.util.Scanner;
+public class Demo02 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入您的薪水");
+        int salary=sc.nextInt();
+        while (true){
+            if(salary>=20000){
+                System.out.println("教授");
+            }else if (salary>=10000&&salary<20000){
+                System.out.println("副教授");
+            }else if (salary>=5000&&salary<10000){
+                System.out.println("讲师");
+            }else if (salary>=2000&&salary<5000){
+                System.out.println("助教");
+            }else if(salary>0&&salary<2000){
+                System.out.println("清洁工");
+            }else {
+                System.out.println("nb");
+            }
+        }
+
+    }
+}
