def5f0afcd6314d247d1870b1eef7c93d2fc8338
Beet-le <1693332907@qq.com>
2021-09-10 09:05:38 +0800

    day910

--- src/Day909/ShuiXianhua.java
+++ src/Day909/ShuiXianhua.java
@@ -1,11 +1,14 @@
 package Day909;
+
 import java.util.Scanner;
+
 public class ShuiXianhua {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("请输入一个三位数");
         int num = scanner.nextInt();
         int a = num / 100;
+        System.out.println(a);
         int b = num / 10 % 10;
         int c = num % 10;
         if (a * a * a + b * b * b + c * c * c == num) {
---
+++ src/Day909/Shuixiahu2.java
@@ -0,0 +1,18 @@
+package Day909;
+
+public class Shuixiahu2 {
+    public static void main(String[] args) {
+
+        for (int i = 100; i < 1000; i++) {
+            int a = i / 100;
+            int b = i / 10 % 10;
+            int c = i % 10;
+            if (a * a * a + b * b * b + c * c * c == i) {
+                System.out.println("他是水仙花数"+i);
+            }
+           /*else {
+                System.out.println("a");
+            }*/
+        }
+    }
+}
