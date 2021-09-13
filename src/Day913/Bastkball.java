07cac8db8d26c51639705703b8f442b322c81a34
Beet-le <1693332907@qq.com>
2021-09-13 15:35:59 +0800

    day913

---
+++ src/Day913/Bastkball.java
@@ -0,0 +1,11 @@
+package Day913;
+
+public class Bastkball {
+    public static void main(String[] args) {
+        double H=10;
+        for (int i = 0; i < 5; i++) {
+            H/=2;
+        }
+        System.out.println("第五次高度："+H);
+    }
+}
---
+++ src/Day913/taoxing.java
@@ -0,0 +1,7 @@
+package Day913;
+
+public class taoxing {
+    public static void main(String[] args) {
+
+    }
+}
