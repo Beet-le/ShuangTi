8f4aa84ee294bda063f5f4c97e06039c4acb656c
Beet-le <1693332907@qq.com>
2021-09-13 16:13:14 +0800

    day913

---
+++ src/Day913/FangXing.java
@@ -0,0 +1,13 @@
+package Day913;
+
+public class FangXing {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+
+            for (int i1 = 0; i1 <=4; i1++) {
+                System.out.print("  *");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/Day913/taoxing.java
+++
@@ -1,7 +0,0 @@
-package Day913;
-
-public class taoxing {
-    public static void main(String[] args) {
-
-    }
-}
