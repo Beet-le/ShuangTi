17b94c39b187788ad3f358b997f8226352b575fa
Beet-le <1693332907@qq.com>
2021-09-15 15:13:02 +0800

    day914

--- src/Day915/Demo03MySz3.java
+++ src/Day915/Demo03MySz3.java
@@ -11,6 +11,12 @@ public class Demo03MySz3 {
         if(i==3){
             newstu[i]=85;
         }
+        for ( i =3; i <newstu.length; i++) {
+            newstu[i+1]=oldstu[i];
+        }
+        for (int i1 = 0; i1 < newstu.length; i1++) {
+            System.out.println(newstu[i1]);
+        }
 
     }
 }
