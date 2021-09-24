34a7dfa936ff3f0a3c2b9671bdefc51c9fc127b5
Beet-le <1693332907@qq.com>
2021-09-24 22:39:35 +0800

    Day919

---
+++ src/Day923/moudle/Main.java
@@ -0,0 +1,18 @@
+package Day923.moudle;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+//        StudentsMoudle studentsMoudle=new StudentsMoudle();
+        System.out.println("用户名");
+        String name=sc.next();
+      /*  studentsMoudle.setName(name);
+        System.out.println("密码");
+        String paseward=sc.next();
+        studentsMoudle.setPaseward(paseward);
+        System.out.println(studentsMoudle.getName());
+        System.out.println(studentsMoudle.getPaseward());*/
+    }
+}
--- src/Day923/moudle/StudentsMoudle.java
+++ src/Day923/moudle/StudentsMoudle.java
@@ -1,4 +1,47 @@
 package Day923.moudle;
 
 public class StudentsMoudle {
+    private int id;
+    private String name;
+    private String paseward;
+    private String sex;
+
+    public StudentsMoudle(int id, String name, String paseward, String sex) {
+        this.id = id;
+        this.name = name;
+        this.paseward = paseward;
+        this.sex = sex;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPaseward() {
+        return paseward;
+    }
+
+    public void setPaseward(String paseward) {
+        this.paseward = paseward;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
