850a1210fc65c6473c521421d4d0992f65df532d
Beet-le <1693332907@qq.com>
2021-09-29 09:35:39 +0800

    Day929

---
+++ src/Day929/Demo02.java
@@ -0,0 +1,11 @@
+package Day929;
+
+public class Demo02 {
+    public static void main(String[] args) {
+        int[] num=new int[5];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
